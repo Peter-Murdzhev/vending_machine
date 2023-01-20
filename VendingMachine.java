b98e17d6533554a64b15fb65f5cfa66a3edb39e2
Peter-Murdzhev <116828503+Peter-Murdzhev@users.noreply.github.com>
2023-01-20 22:42:18 +0200

    Add files via upload

---
+++ VendingMachine.java
@@ -0,0 +1,49 @@
+package vending_machine;
+
+import java.util.Scanner;
+
+public class VendingMachine {
+    public void machineOperator(Product product, Inventory inventory) {
+        Scanner scan = new Scanner(System.in);
+
+        if (product.getAmount() <= 0) {
+            System.out.println("The product is out of stock.");
+            return;
+        }
+
+        System.out.print("Input coin or type stop: ");
+        String input = scan.nextLine();
+        double currentSum = 0;
+
+        while (currentSum < product.getPrice() && !input.equals("stop")) {
+            try {
+                currentSum += Coin.coins.get(Integer.parseInt(input));
+            } catch (NullPointerException ex) {
+                System.out.println("Not valid coin!");
+            } catch (NumberFormatException ex) {
+                System.out.println("Not a number!");
+            }
+
+            if (currentSum < product.getPrice()) {
+                System.out.print("Input coin or type stop: ");
+                input = scan.nextLine();
+            }
+        }
+
+        if (currentSum < product.getPrice()) {
+            System.out.println("The money isn't enough!");
+            System.out.println("Returning inserted cash: " + currentSum);
+            return;
+        }
+
+        if (inventory.getCash() < (currentSum - product.getPrice())) {
+            System.out.println("The device doesn't have cash to return change.");
+            System.out.println("Returning inserted cash: " + currentSum);
+            return;
+        }
+
+        product.prepareProduct(currentSum);
+        inventory.updateCash(currentSum);
+        System.out.println();
+    }
+}
